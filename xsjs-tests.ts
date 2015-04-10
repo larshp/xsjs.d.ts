46743df93687d9fa7ef7f153a81f3cb2898b12aa
Lars Hvam <larshp@hotmail.com>
2015-04-10 20:48:29 +0200

    Update xsjs-tests.ts

--- xsjs-tests.ts
+++ xsjs-tests.ts
@@ -14,6 +14,7 @@ var test: number = $.db.isolation.READ_COMMITTED;
 $.response.contentType = "text/html";
 var output = "Hello World!<br><br>";
 var conn = $.db.getConnection();
+
 var pstmt = conn.prepareStatement("select * from DUMMY");
 var rs = pstmt.executeQuery();
 if (!rs.next()) {
@@ -44,54 +45,14 @@ for (i = 0; i < $.request.entities.length; ++i) {
 }
 
 if ($.request.method === $.net.http.GET) {
-    // get query parameter named id
     var qpId = $.request.parameters.get("id");
 
-    // send response
     $.response.contentType = "plain/test";
     $.response.setBody("body");
     $.response.status = $.net.http.OK;
 } else {
-    // unsupported method
     $.response.status = $.net.http.INTERNAL_SERVER_ERROR;
 }
-
-//create email from JS Object and send
-var mail = new $.net.Mail({
-    sender: { address: "demo@sap.com" },
-    to: [{ address: "demo@sap.com" }],
-    subject: "XSJS Email Test",
-    parts: [new $.net.Mail.Part({
-        type: $.net.Mail.Part.TYPE_TEXT,
-        text: "The body of the mail.",
-        contentType: "text/plain"
-    })]
-});
-var returnValue = mail.send();
-var response = "MessageId = " + returnValue.messageId + ", final reply = " + returnValue.finalReply;
-
-
-//create email from JS Object and send
-var mail = new $.net.Mail({
-    sender: { address: "demo@sap.com" },
-    to: [{ address: "demo@sap.com" }],
-    subject: "XSJS Email Test",
-    parts: [new $.net.Mail.Part({
-        type: $.net.Mail.Part.TYPE_TEXT,
-        text: "Atachement Test",
-        contentType: "text/plain"
-    })]
-});
-mail.parts.push(new $.net.Mail.Part({
-    type: $.net.Mail.Part.TYPE_ATTACHMENT,
-    data: getImage(),
-    contentType: "image/jpg",
-    fileName: "myPicture.jpg"
-}));
-var returnValue = mail.send();
-var response = "MessageId = " + returnValue.messageId +
-    ", final reply = " + returnValue.finalReply;
-
 
 var zip = new $.util.Zip();
 zip["folder1/demo1.txt"] = "This is the new ZIP Processing in XSJS";
@@ -101,22 +62,17 @@ $.response.contentType = "application/zip";
 $.response.headers.set('Content-Disposition', "attachment; filename = 'ZipExample.zip'");
 $.response.setBody(zip.asArrayBuffer());
 
-
 var zip = new $.util.Zip($.request.body);
 
-
-statement = conn.prepareStatement("select data from EXAMPLETABLE where id=1");
+var statement = conn.prepareStatement("select data from EXAMPLETABLE where id=1");
 var rs = statement.executeQuery();
 if (rs) {
     while (rs.next()) {
-        //Load Zip From ResultSet
         var loadedZip = new $.util.Zip(rs, 1);
     }
 }
 
-//create a new $.util.SAXParser object
 var parser = new $.util.SAXParser();
-//parse XML from String
 var parser = new $.util.SAXParser();
 var xml = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>\n' +
     '<!-- this is a note -->\n' +
@@ -150,8 +106,6 @@ $.response.status = $.net.http.OK;
 $.response.contentType = "text/html";
 $.response.setBody(body);
 
-
-// Handling of multipart requests and responses in xsjs files:
 var i;
 var n = $.request.entities.length;
 var client = new $.net.http.Client();
@@ -162,8 +116,6 @@ for (i = 0; i < n; ++i) {
     var responseEntity = $.response.entities.create();
     responseEntity.setBody(childResponse);
 }
-
-
 
 $.response.contentType = "text/html";
 var output = "Hello, World! <br><br>";
@@ -187,25 +139,12 @@ else {
     });
 }
 
-
-var textAccess = $.import("sap.hana.xs.i18n", "text");
-var bundle = textAccess.loadBundle("playground.sp9.textAccess", "demo1");
-var singleText = bundle.getText("demo");
-var replaceText = bundle.getText("demo2", ['1001']);
-var oAllTexts = bundle.getTexts();
-//$.response.setBody(singleText);
-$.response.setBody(replaceText);
-//$.response.setBody(JSON.stringify(oAllTexts));
-
-
 try {
-    //create a new $.security.AntiVirus object using the default profile
     var av = new $.security.AntiVirus();
     av.scan($.request.body);
 } catch (e) {
     $.response.setBody(e.toString());
 }
-
 
 function store() {
     var config = {
@@ -241,20 +180,10 @@ switch (aCmd) {
         $.response.setBody('Invalid Command');
 }
 
-// create client
 var client = new $.net.http.Client();
-
-// where and what to send
 var dest = $.net.http.readDestination("testApp", "myDestination");
-var request = new $.net.http.Request($.net.http.GET, "/"); // new Request(METHOD, PATH)
-                                                           // the PATH will be prefixed by destination's pathPrefix, e.g. "/search?" on the request
-
-// send the request and synchronously get the response
-client.request(request, dest);
 var response2 = client.getResponse();
 
-
-// get all the cookies and headers from the response
 var co = [], he = [];
 for(var c in response2.cookies) {
     co.push(response2.cookies[c]);
@@ -264,12 +193,32 @@ for(var c in response2.headers) {
      he.push(response2.headers[c]);
 }
 
-// get the body
 if(!response2.body)
     body = "";
 else
     body = response2.body.asString();
 
-// send the response as JSON
 $.response.contentType = "application/json";
 $.response.setBody(JSON.stringify({"status": response2.status, "cookies": co, "headers": he, "body": body}));
+
+
+var mail = new $.net.Mail();
+mail.subject = "About what the email is."
+mail.subjectEncoding = "UTF-8";
+mail.sender = {address: "from@sap.com"};
+mail.cc = [{name: "Cc1", address: "cc1@recepient.com"}, {address: "cc2@recepient.com"}];
+
+var response = "";
+
+try {
+    var returnValue = mail.send();
+} catch(error) {
+    response = "Error occurred:" + error.message;
+}
+response = "MessageId = " + returnValue.messageId + ", final reply = " + returnValue.finalReply;
+
+$.response.status = $.net.http.OK;
+$.response.contentType = "text/html";
+$.response.setBody(response);
+
+var foosdf = $.net.Mail.Part.TYPE_TEXT;
